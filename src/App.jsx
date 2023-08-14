da9577366444f2b412826fd8ed37f35a1d350d50
kingbookk <kingbooktashkent@gmail.com>
2023-08-14 11:53:45 +0500

    branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,60 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
-import  Pet from './components/Pet';
-import Search from "./components/Search";
-import Counter from "./components/Counter";
+import Loading from "./components/Loading";
 
 
-
-
-
+// const url = 'https://course-api.com/react-tours-project'
+const url = 'https://jsonplaceholder.typicode.com/posts'
 
 
 const App = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [travels, setTravels] = useState([]); 
+
+
+  const removeItem = (id) => {
+    const newItem = travels.filter((item) => item.id !== id);
+    setTravels(newItem)
+  }
+
+
+
+  const fetchTours = async () => {
+    setLoading(true)
+    try {
+      const resp = await fetch(url)
+      const data = await resp.json()
+      setLoading(false)
+      setTravels(data)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
+  if (loading) {
+    return <Loading />
+  }
+
+
+
+
   return (
     <div>
       <h1>hello world</h1>
-      {/* <Search /> */}
-      {/* <Pet name="chihua" type="oddiy" meal="suyak" /> */}
-      <Counter />
+      {travels.map((travel) => {
+        return (
+          <div key={travel.id}>
+            {travel.title}
+            <button onClick={removeItem(travel.id)}>remove</button>
+         </div>
+       )
+     })}
     </div>
   )
 }
